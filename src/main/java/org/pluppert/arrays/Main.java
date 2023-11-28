d8ce5e36258cac187482aa82c679870c4ee73002
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-28 09:56:47 +0100

    - Do Exercise 1

--- src/main/java/org/pluppert/arrays/Main.java
+++ src/main/java/org/pluppert/arrays/Main.java
@@ -3,5 +3,11 @@ package org.pluppert.arrays;
 public class Main {
     public static void main(String[] args) {
 
+        // Exercise 1:
+        int[] array = {11, 23, 39, 12, 103, 79, 84};
+        System.out.println("Numbers in array of int values:");
+        for(int number : array) {
+            System.out.print(number + " ");
+        }
     }
 }
