1dfa06ee9d14d6e57aef6a303192894fb858b827
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-28 10:14:16 +0100

    - Do Exercise 2

--- src/main/java/org/pluppert/arrays/Main.java
+++ src/main/java/org/pluppert/arrays/Main.java
@@ -5,9 +5,24 @@ public class Main {
 
         // Exercise 1:
         int[] array = {11, 23, 39, 12, 103, 79, 84};
+        /*
         System.out.println("Numbers in array of int values:");
         for(int number : array) {
             System.out.print(number + " ");
         }
+        */
+
+        // Exercise 2:
+        System.out.println("Index position of number" + " 12 -> " + indexOf(array, array[3]));
+    }
+
+    // Exercise 2:
+    static int indexOf(int[] array, int elementOfInt) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == elementOfInt) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
